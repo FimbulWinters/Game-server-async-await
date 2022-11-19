a9be11b84e5d8c92f1147c8892a7b7c2cf9f0af7
FimbulWinters <dakin.92@outlook.com>
2022-11-19 14:59:46 +0000

    Convert updateVotes to async await

--- Models/patchModel.js
+++ Models/patchModel.js
@@ -1,20 +1,19 @@
 const db = require("../db/connection.js");
 const { doesReviewExist } = require("../db/seeds/utils.js");
 
-exports.updateVotes = (id, voteAction) => {
-  return doesReviewExist(id)
-    .then(() => {
-      return db.query(`SELECT votes FROM reviews WHERE review_id = $1`, [id]);
-    })
-    .then((res) => {
-      const currentVotes = res.rows[0].votes;
-      const newVotes = currentVotes + voteAction.inc_votes;
-      return db.query(
-        `UPDATE reviews SET votes = $1 WHERE review_id = $2 RETURNING *`,
-        [newVotes, id],
-      );
-    })
-    .then((result) => {
-      return result.rows[0];
-    });
+exports.updateVotes = async (id, voteAction) => {
+  await doesReviewExist(id);
+
+  const res = await db.query(`SELECT votes FROM reviews WHERE review_id = $1`, [
+    id,
+  ]);
+
+  const currentVotes = res.rows[0].votes;
+  const newVotes = currentVotes + voteAction.inc_votes;
+  const result = await db.query(
+    `UPDATE reviews SET votes = $1 WHERE review_id = $2 RETURNING *`,
+    [newVotes, id],
+  );
+
+  return result.rows[0];
 };
