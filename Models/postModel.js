f38d027884390cf54c7240244f69388867d9107b
FimbulWinters <dakin.92@outlook.com>
2022-11-19 15:02:54 +0000

    Convert insertComment to async await

--- Models/postModel.js
+++ Models/postModel.js
@@ -5,19 +5,16 @@ const {
   doesUsernameExist,
 } = require("../db/seeds/utils");
 
-exports.insertComment = (id, data) => {
+exports.insertComment = async (id, data) => {
   const { body, username } = data;
-  return doesReviewExist(id)
-    .then(() => {
-      return doesUsernameExist(username);
-    })
-    .then(() => {
-      return db.query(
-        `INSERT INTO comments (body, author, review_id) VALUES ($1,$2,$3) RETURNING *;`,
-        [body, username, id],
-      );
-    })
-    .then((res) => {
-      return res.rows[0];
-    });
+  await doesReviewExist(id);
+
+  await doesUsernameExist(username);
+
+  const result = await db.query(
+    `INSERT INTO comments (body, author, review_id) VALUES ($1,$2,$3) RETURNING *;`,
+    [body, username, id],
+  );
+
+  return result.rows[0];
 };
